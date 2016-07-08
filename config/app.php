6b2fecde71815d0278573757b66ed21b59de6460
victor <victor.rojas@ec.pe>
2016-07-07 19:39:09 -0500

    install

--- config/app.php
+++ config/app.php
@@ -158,8 +158,11 @@ return [
 
         /*  Html Provider */
         Collective\Html\HtmlServiceProvider::class,
-
         Intervention\Image\ImageServiceProvider::class,
+        Laracasts\Flash\FlashServiceProvider::class,
+        Prettus\Repository\Providers\RepositoryServiceProvider::class,
+        \InfyOm\Generator\InfyOmGeneratorServiceProvider::class,
+        \InfyOm\CoreTemplates\CoreTemplatesServiceProvider::class,
 
     ],
 
@@ -208,7 +211,8 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
-        'Image' => Intervention\Image\Facades\Image::class
+        'Image' => Intervention\Image\Facades\Image::class,
+        'Flash'     => Laracasts\Flash\Flash::class,
         
     ],
 
